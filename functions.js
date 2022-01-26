c173edf1c11a49a22122e9011cfb5eded3b71735
HugoRo <ndhugohro@gmail.com>
2022-01-25 21:35:14 -0600

    añadiendo actualización

--- functions.js
+++ functions.js
@@ -18,3 +18,21 @@ optionTwo.textContent = "Popularity (descendente)";
 optionTwo.value = "pupularity.desc"
 select.appendChild(optionTwo);
 
+select.onchange = () => {
+    makeImages(divGeneral);
+};
+
+makeImages(divGeneral);
+
+
+
+let year = document.createElement("select");
+let option01 = document.createElement("option");
+option01.textContent = "Popularity (as)"
+
+select.onchange = (1900) => (2022) {
+    makeImages(divGeneral);
+};
+
+
+
