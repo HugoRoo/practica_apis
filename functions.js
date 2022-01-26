02e1cf64d9e0e5d78694a655d1d3d02887b53050
HugoRo <ndhugohro@gmail.com>
2022-01-25 21:30:52 -0600

    subiendo función

---
+++ functions.js
@@ -0,0 +1,20 @@
+
+/* Array*/
+
+let elementsHTML = [];
+
+let divGeneral = document.createElement("div");
+
+
+let select = document.createElement("select");
+let optionOne = document.createElement("option");
+optionOne.textContent = "Popularity (descendente)";
+optionOne.value = "pupularity.desc"
+select.appendChild(optionOne);
+
+let select = document.createElement("select");
+let optionTwo = document.createElement("option");
+optionTwo.textContent = "Popularity (descendente)";
+optionTwo.value = "pupularity.desc"
+select.appendChild(optionTwo);
+
